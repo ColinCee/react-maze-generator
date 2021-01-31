dbca4c601810f08e07fe2bffd9fc91fda6a1bd2f
Colin Cheung <ColinCee@users.noreply.github.com>
2021-01-31 13:09:06 +0000

    fix border issue

--- src/components/grid/Cell.jsx
+++ src/components/grid/Cell.jsx
@@ -6,7 +6,7 @@ type Props = {
   node: Node,
 };
 
-const Cell = ({ node }: Props) => {
+const generateStyle = (node: Node) => {
   const style = {};
   if (node.walls.N) {
     style.borderTop = "2px solid gray";
@@ -15,8 +15,20 @@ const Cell = ({ node }: Props) => {
   if (node.walls.E) {
     style.borderRight = "2px solid gray";
   }
-  return <div className="cell" style={style} />;
+
+  if (!node.connections.W) {
+    style.borderLeft = "2px solid gray";
+  }
+
+  if (!node.connections.S) {
+    style.borderBottom = "2px solid gray";
+  }
+
+  return style;
 };
+const Cell = ({ node }: Props) => (
+  <div className="cell" style={generateStyle(node)} />
+);
 
 Cell.propTypes = {
   node: PropTypes.instanceOf(Node).isRequired,
